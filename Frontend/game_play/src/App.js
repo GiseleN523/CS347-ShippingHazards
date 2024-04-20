aabd1dc1bbe493cadf2de5714c2ad22b8ea9f13d
GiseleN523 <giselenelson@rocketmail.com>
2024-04-20 14:04:25 -0500

    started on grids

--- Frontend/game_play/src/App.js
+++ Frontend/game_play/src/App.js
@@ -1,5 +1,6 @@
 import logo from './images/logo.png';
 import './App.css';
+import './GamePlay.css';
 //import { useState } from 'react';
 /*
 function Navigation(){
@@ -15,6 +16,48 @@ function Navigation(){
   )
 }*/
 
+function GridSquare() {
+  return (
+    <div className="grid-square"></div>
+  )
+}
+
+function GridRow(size) {
+  size = 10
+  let result = [];
+  for (let i = 0; i < size; i++) {
+    result.push(<GridSquare />);
+  }
+  return (
+    <div id="grid-row">{result}</div>
+  )
+}
+
+function Grid(size) {
+  size = 10
+  let result = [];
+  for (let i = 0; i < size; i++) {
+    result.push(<GridRow />);
+  }
+  console.log(result)
+  return (
+    <div id="grid">{result}</div>
+  )
+}
+
+function GamePlay() {
+  return (
+    <Grid />
+  )
+}
+
+function InstructionBox() {
+  return (
+    <div id="instruction-box" className="box">
+      Your Turn: Choose a spot to attack!
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -40,6 +83,8 @@ function App() {
           </li>
         </ul>
       </nav>
+      <InstructionBox />
+      <GamePlay />
     </div>
   );
 }
