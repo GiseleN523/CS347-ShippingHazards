f625ed3a9477671189834c953e44dfc7cea82bd1
GiseleN523 <giselenelson@rocketmail.com>
2024-04-20 15:02:28 -0500

    grid and background image

--- Frontend/game_play/src/App.js
+++ Frontend/game_play/src/App.js
@@ -1,5 +1,6 @@
 import logo from './images/logo.png';
 import './App.css';
+import './GamePlay.css'
 
 function GridSquare() {
   return (
@@ -30,49 +31,75 @@ function Grid(size) {
   )
 }
 
-function GamePlay() {
+function Instructions() {
   return (
-    <Grid />
+    <div id="gameplay-instructions">
+      Your Turn: Choose a spot to attack!
+    </div>
   )
 }
 
-function InstructionBox() {
+function GamePlay() {
   return (
-    <div id="instruction-box" className="box">
-      Your Turn: Choose a spot to attack!
+    <div id="game-content">
+      <Instructions />
+      <div id="boards">
+        <div id="boards-row">
+          <div className="boards-cell">YOUR BOARD</div>
+          <div className="boards-cell">OPPONENT BOARD</div>
+        </div>
+        <div id="boards-row">
+          <div className="boards-cell">
+            <Grid />
+          </div>
+          <div className="boards-cell">
+            <Grid />
+          </div>
+        </div>
+      </div>
     </div>
+  )
+}
+
+function Header() {
+  return (
+    <header className="App-Header">
+      <div className="logo">
+        <img src= {logo} alt="Logo" />
+      </div>
+      <div className="header-text">
+        <h1>SHIPPING HAZARDS: A Game By Pink Puffy Rhinos</h1>
+      </div>
+    </header>
+  )
+}
+
+function NavigationBar() {
+  return (
+    <nav className="App-Navigation">
+      <ul className="nav-list">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about_us">About Us</a>
+        </li>
+        <li>
+          <a href="#my_account">My Account/Log Out</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
 function App() {
   return (
     <div>
-      <header className="App-Header">
-        <div className="logo">
-          <img src= {logo} alt="Logo" />
-        </div>
-        <div className="header-text">
-          <h1>SHIPPING HAZARDS: A Game By Pink Puffy Rhinos</h1>
-        </div>
-      </header>
-      <nav className="App-Navigation">
-        <ul className="nav-list">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about_us">About Us</a>
-          </li>
-          <li>
-            <a href="#my_account">My Account/Log Out</a>
-          </li>
-        </ul>
-      </nav>
-      <InstructionBox />
+      <Header />
+      <NavigationBar />
       <GamePlay />
     </div>
   );
 }
-
 
 export default App;
