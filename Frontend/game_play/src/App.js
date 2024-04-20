bd2e34e3b211d8f43cbb57d97c60e1741b2e9300
Cece Che Tita <cecechetita@Ceces-MacBook-Pro.local>
2024-04-20 14:01:20 -0500

    Updated Headr with icon and nav bar

--- Frontend/game_play/src/App.js
+++ Frontend/game_play/src/App.js
@@ -1,19 +1,5 @@
 import logo from './images/logo.png';
 import './App.css';
-//import { useState } from 'react';
-/*
-function Navigation(){
-  return (
-    <nav class="App-Navigation">
-      <img src={logo} alt="logo" />
-      <p1>SHIPPING HAZARDS: A game by Pink Puffy Rhinos</p1>
-      <li>Home</li>
-      <li>About Us</li>
-      <li>My Account/Log Out</li>
-    </nav>
-
-  )
-}*/
 
 
 function App() {
@@ -21,7 +7,7 @@ function App() {
     <div>
       <header className="App-Header">
         <div className="logo">
-          <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTJ_MAD51R5iBkzA6xH7gLe5gAtXYxzJvQxHfvccT7yjXfru-sX" alt="Logo" />
+          <img src= {logo} alt="Logo" />
         </div>
         <div className="header-text">
           <h1>SHIPPING HAZARDS: A Game By Pink Puffy Rhinos</h1>
@@ -43,30 +29,6 @@ function App() {
     </div>
   );
 }
-/*  function App() {
-    return (
-      <div>
-        <Navigation></Navigation>
-        <div className="App">
-          <header className="App-header">
-            {/* <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a> }
-          </header>
-        </div>
-      </div>
-    );
-  }*/
-
 
 
 export default App;
