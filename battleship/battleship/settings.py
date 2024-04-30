4fb61581f9a98f52565b87b30f31f357bdc21725
RyanSDunn <dunnr@carleton.edu>
2024-04-29 19:54:35 -0500

    Added Docker

--- battleship/battleship/settings.py
+++ battleship/battleship/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'battleship.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
