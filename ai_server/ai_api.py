9fa315f7871b3bc126706cb53a5fd78ced92e6b7
josh-meier <joshmeier456@gmail.com>
2024-05-03 15:22:11 -0500

    changed requests.get() lines to use str formatting and not params

--- ai_server/ai_api.py
+++ ai_server/ai_api.py
@@ -21,19 +21,21 @@ def start_new_game(player1_id, player2_id, num_ships, board_size, game_id):
 def run_game(player1_id, player2_id, num_ships, board_size, game_id):
     # setup the ships
     ship_board = "-a---------a--------------------cccc-----------d---------d---------d---------d------bbb-------------"
-    params = {'game_id': game_id,
-              'player2_id': player2_id,
-              'ship_board': ship_board}
-    response = requests.get('http://web:8000/confirm-ships/{{game_id}}/{{player2_id}}/{{ship_board}}', params)
+    # params = {'game_id': game_id,
+    #           'player2_id': player2_id,
+    #           'ship_board': ship_board}
+    url = 'http://web:8000/confirm-ships/{}/{}/{}'.format(game_id, player2_id, ship_board)
+    response = requests.get(url)
     # initialize the ai player
     ai = BattleShipAI()
     ai.attackBoard = "-"*100
     ai.type = ai_player_ids[player2_id]    # set its type to the corresponding player2_id
     
     # call get_state
-    params = {'game_id': game_id,
-              'player2_id': player2_id}
-    response = requests.get('http://web:8000/get-state/{{game_id}}/{{player2_id}}/false', params)
+    # params = {'game_id': game_id,
+    #           'player2_id': player2_id}
+    url = 'http://web:8000/get-state/{}/{}/false'.format(game_id, player2_id)
+    response = requests.get(url)
     data = response.json()
     # save game_status, my_turn, and attack_board in vars
     ai.attackBoard = data["attack_board"] 
@@ -43,9 +45,10 @@ def run_game(player1_id, player2_id, num_ships, board_size, game_id):
     # while the game isnt over (game_status == 0) 
     while game_status == 0:
         time.sleep(5)
-        params = {'game_id': game_id,
-              'player2_id': player2_id}
-        response = requests.get('http://web:8000/get-state/{{game_id}}/{{player2_id}}/false', params)
+        # params = {'game_id': game_id,
+        #       'player2_id': player2_id}
+        url = 'http://web:8000/get-state/{}/{}/false'.format(game_id, player2_id)
+        response = requests.get(url)
         data = response.json()
         ai.attackBoard = data["attack_board"] 
         my_turn = data["turn"]
@@ -55,18 +58,19 @@ def run_game(player1_id, player2_id, num_ships, board_size, game_id):
         ai.previousShotCol = data["shot_col"]
         if my_turn == 2:
             row, col = ai.getMove()
-            params = {'game_id': game_id,
-                    'player2_id': player2_id,
-                    'row': row,
-                    'col': col}
+            # params = {'game_id': game_id,
+            #         'player2_id': player2_id,
+            #         'row': row,
+            #         'col': col}
             # do we need to sleep here??
             time.sleep(5)
-            response = requests.get('http://web:8000/fire-shot/{{game_id}}/{{player2_id}}/{{row}}/{{col}}', params)
+            url = 'http://web:8000/fire-shot/{}/{}/{}/{}'.format(game_id, player2_id, row, col) 
+            response = requests.get(url)
      
     return
 
 
 if __name__ == '__main__':
     host = '0.0.0.0'
-    port = 7000 # do we want to use this port?
+    port = 7000 
     app.run(host=host, port=port, debug=True)
