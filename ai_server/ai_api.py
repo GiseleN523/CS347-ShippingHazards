477df9455e50ec872f617978ebba4fa9401a41cb
josh-meier <joshmeier456@gmail.com>
2024-05-16 18:56:40 -0500

    updated ai_server to have ai actually make shots

--- ai_server/ai_api.py
+++ ai_server/ai_api.py
@@ -25,19 +25,39 @@ def start_new_game(player1_id, player2_id, num_ships, board_size, game_id):
     
     return ""
 
+def update_ai(ai, inner_dict):
+    ai.attackBoard = inner_dict["attack_board"] 
+    ai.previousShotHit = inner_dict["is_hit"]
+    ai.previousShotRow = inner_dict["shot_row"]
+    ai.previousShotCol = inner_dict["shot_col"]
+           
+
 def start_ai(player1_id, player2_id, num_ships, board_size, game_id):
-    
+    # initialize ai outside of ws functions so the ai object can be accessed 
+    ai = BattleShipAI()
+    ai.attackBoard = "-"*100
+    ai.type = ai_player_ids[player2_id]
+
     def on_message(ws, message):
         sys.stderr.write("AI Received: " + message + "/n")
         message_dict = json.loads(message)
         inner_dict = json.loads(message_dict['message'])
-        if (inner_dict['turn'] == 2):
-            row = random.randint(0,6)
-            col = random.randint(0,6)
-            url = 'http://web:8000/play/fire-shot/{}/{}/{}/{}'.format(game_id, player2_id, row, col)
-            response = requests.get(url)
+        
+        game_status = inner_dict["status"]
+        if game_status == 0: # while the game is not over (I think its an if and not a while now)
+            
+            update_ai(ai, inner_dict)
+
+            if (inner_dict['turn'] == 2):
+                # row = random.randint(0,6) ### for testing purposes
+                # col = random.randint(0,6)
+
+                row, col = ai.getMove()
+                url = 'http://web:8000/play/fire-shot/{}/{}/{}/{}'.format(game_id, player2_id, row, col)
+                response = requests.get(url)
 
     def on_open(ws):
+        # ai "selects" its ship placement (will eventually have shipBoard be different every time)
         ship_board = "-a---------a--------------------cccc-----------d---------d---------d---------d------bbb-------------"
         url = 'http://web:8000/play/confirm-ships/{}/{}/{}'.format(game_id, player2_id, ship_board)
         response = requests.get(url)
