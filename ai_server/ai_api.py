83e3b1bf8d28da367ecbcf896fbd363d85448e31
josh-meier <joshmeier456@gmail.com>
2024-05-21 01:18:14 -0500

    brought over changes from ai_updates to a new branch from the most recent main
    still in the testing process. believe that ai 1 and 2 are working, but ai 3 breaks because there are some out of bounds indexing that we need to account for inside betterTargetedAttack()

--- ai_server/ai_api.py
+++ ai_server/ai_api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import requests
 import threading
-from ai_player.ai import BattleShipAI
+from ai_player.ai import *
 import time
 # Now using this library:
 #   https://websocket-client.readthedocs.io/en/latest/index.html
@@ -12,14 +12,14 @@ import random
 
 app = Flask(__name__)
 
-ai_player_ids = {"1": "inOrder",
-                "2": "random",
-                "3": "targeted"}
+ai_player_ids = {"1": "random",
+                "2": "targeted",
+                "3": "best"}
 
 # not sure if this route is right?? 
 @app.route('/new-game/<player1_id>/<player2_id>/<num_ships>/<board_size>/<game_id>') 
 def start_new_game(player1_id, player2_id, num_ships, board_size, game_id):
-    
+    print("opening thread", flush=True)
     ai_thread = threading.Thread(target=start_ai, args=(player1_id, player2_id, num_ships, board_size, game_id))
     ai_thread.start()
     
@@ -30,12 +30,17 @@ def update_ai(ai, inner_dict):
     ai.previousShotHit = inner_dict["is_hit"]
     ai.previousShotRow = inner_dict["shot_row"]
     ai.previousShotCol = inner_dict["shot_col"]
+    ai.previousShotSunk = inner_dict["is_sunk"]
+    ai.opponentCombinedBoard = inner_dict["combined_board"]
+    ai.opponentShipBoard = inner_dict["ship_board"] ## this might be the wrong board??
            
 
 def start_ai(player1_id, player2_id, num_ships, board_size, game_id):
     # initialize ai outside of ws functions so the ai object can be accessed 
     ai = BattleShipAI()
     ai.attackBoard = "-"*100
+    ai.numShips = int(num_ships)
+    ai.boardSize = int(board_size)
     ai.type = ai_player_ids[player2_id]
 
     def on_message(ws, message):
@@ -48,25 +53,32 @@ def start_ai(player1_id, player2_id, num_ships, board_size, game_id):
 
             # if the message from the websocket has information about the correct player's board, update the ai object
             if (int(inner_dict["player_id"]) != int(player2_id)):
+                print("updating ai", flush=True)
                 update_ai(ai, inner_dict)
 
             if (inner_dict['turn'] == 2):
                 # row = random.randint(0,6) ### for testing purposes
                 # col = random.randint(0,6)
+                print("trying to get a move", flush=True)
 
                 row, col = ai.getMove()
+                print("got the move", flush=True)
                 url = 'http://web:8000/play/fire-shot/{}/{}/{}/{}'.format(game_id, player2_id, row, col)
                 response = requests.get(url)
 
     def on_open(ws):
         # ai "selects" its ship placement (will eventually have shipBoard be different every time)
-        ship_board = "-a---------a--------------------cccc-----------d---------d---------d---------d------bbb-------------"
+        # ship_board = "-a---------a--------------------cccc-----------d---------d---------d---------d------bbb-------------"
+        print("trying to place ships", flush=True)
+        ship_board = placeShips(int(num_ships), int(board_size))
+        print(ship_board, flush=True)
         url = 'http://web:8000/play/confirm-ships/{}/{}/{}'.format(game_id, player2_id, ship_board)
 
         response = requests.get(url)
 
     ###############################
     #BODY of start_ai function
+    print("trying to connect to ws", flush=True)
     ws_url = "ws://web:8000/ws/play/{}/".format(game_id) 
     wsapp = websocket.WebSocketApp(ws_url, on_message=on_message, on_open=on_open)
     wsapp.run_forever()
--- ai_server/test_ai_player.py
+++ ai_server/test_ai_player.py
@@ -1,6 +1,6 @@
 ## maybe make some boilerplate logic for how a game will go so we can reuse it in the api??
 
-from ai_player.ai import BattleShipAI
+from ai_player.ai import *
 # from game_logic.logic import *
 
 
@@ -99,54 +99,75 @@ def updateBoards(shipBoard, prevCombinedBoard, prevAttackBoard, attackRow, attac
 if __name__ == '__main__':
     
     # test the inOrderAI
-    aiInOrder = BattleShipAI()
-    aiInOrder.attackBoard=blankBoard()
-    aiInOrder.opponentShipBoard = "-----------a---------a------------bbbb----------------c---------c---------c--------------------ddddd"
-    aiInOrder.type = "inOrder"
-    aiInOrder.combinedBoard = aiInOrder.opponentShipBoard
+    # aiInOrder = BattleShipAI()
+    # aiInOrder.attackBoard=blankBoard()
+    # aiInOrder.opponentShipBoard = "-----------a---------a------------bbbb----------------c---------c---------c--------------------ddddd"
+    # aiInOrder.type = "inOrder"
+    # aiInOrder.combinedBoard = aiInOrder.opponentShipBoard
 
-    for location in aiInOrder.attackBoard:
-        row, col = aiInOrder.getMove()
-        aiInOrder.combinedBoard, aiInOrder.attackBoard = updateBoards(aiInOrder.opponentShipBoard, aiInOrder.combinedBoard, aiInOrder.attackBoard, row, col)
-        print("after next shot in order")
-        print ("ship board is still "+ aiInOrder.opponentShipBoard)
-        print ("combined board is now: " + aiInOrder.combinedBoard)
-        print ("attack board is now: " + aiInOrder.attackBoard)
+    # for location in aiInOrder.attackBoard:
+    #     row, col = aiInOrder.getMove()
+    #     aiInOrder.combinedBoard, aiInOrder.attackBoard = updateBoards(aiInOrder.opponentShipBoard, aiInOrder.combinedBoard, aiInOrder.attackBoard, row, col)
+    #     print("after next shot in order")
+    #     print ("ship board is still "+ aiInOrder.opponentShipBoard)
+    #     print ("combined board is now: " + aiInOrder.combinedBoard)
+    #     print ("attack board is now: " + aiInOrder.attackBoard)
 
-    # test the randomAI
+    # # test the randomAI
 
-    aiRandom = BattleShipAI()
-    aiRandom.attackBoard=blankBoard()
-    aiRandom.opponentShipBoard = "-----------a---------a------------bbbb----------------c---------c---------c--------------------ddddd"
-    aiRandom.type = "random"
-    aiRandom.combinedBoard = aiRandom.opponentShipBoard
+    # aiRandom = BattleShipAI()
+    # aiRandom.attackBoard=blankBoard()
+    # aiRandom.opponentShipBoard = "-----------a---------a------------bbbb----------------c---------c---------c--------------------ddddd"
+    # aiRandom.type = "random"
+    # aiRandom.combinedBoard = aiRandom.opponentShipBoard
 
-    for location in aiRandom.attackBoard:
-        row, col = aiRandom.getMove()
-        aiRandom.combinedBoard, aiRandom.attackBoard = updateBoards(aiRandom.opponentShipBoard, aiRandom.combinedBoard, aiRandom.attackBoard, row, col)
-        print("after next shot randomly")
-        print ("ship board is still "+ aiRandom.opponentShipBoard)
-        print ("combined board is now: " + aiRandom.combinedBoard)
-        print ("attack board is now: " + aiRandom.attackBoard)
+    # for location in aiRandom.attackBoard:
+    #     row, col = aiRandom.getMove()
+    #     aiRandom.combinedBoard, aiRandom.attackBoard = updateBoards(aiRandom.opponentShipBoard, aiRandom.combinedBoard, aiRandom.attackBoard, row, col)
+    #     print("after next shot randomly")
+    #     print ("ship board is still "+ aiRandom.opponentShipBoard)
+    #     print ("combined board is now: " + aiRandom.combinedBoard)
+    #     print ("attack board is now: " + aiRandom.attackBoard)
 
-    # test the targeted AI
+    # # test the targeted AI
     
-    aiTargeted = BattleShipAI()
-    aiTargeted.attackBoard=blankBoard()
-    aiTargeted.opponentShipBoard = "-----------a---------a------------bbbb----------------c---------c---------c--------------------ddddd"
-    aiTargeted.type = "targeted"
-    aiTargeted.combinedBoard = aiTargeted.opponentShipBoard
+    # aiTargeted = BattleShipAI()
+    # aiTargeted.attackBoard=blankBoard()
+    # aiTargeted.opponentShipBoard = "-----------a---------a------------bbbb----------------c---------c---------c--------------------ddddd"
+    # aiTargeted.type = "targeted"
+    # aiTargeted.combinedBoard = aiTargeted.opponentShipBoard
 
-    for location in aiTargeted.attackBoard:
-        row, col = aiTargeted.getMove()
-        aiTargeted.combinedBoard, aiTargeted.attackBoard = updateBoards(aiTargeted.opponentShipBoard, aiTargeted.combinedBoard, aiTargeted.attackBoard, row, col)
-        print("after next shot targeted")
-        print ("ship board is still    "+ aiTargeted.opponentShipBoard)
-        print ("combined board is now: " + aiTargeted.combinedBoard)
-        print ("attack board is now:   " + aiTargeted.attackBoard)
+    # for location in aiTargeted.attackBoard:
+    #     row, col = aiTargeted.getMove()
+    #     aiTargeted.combinedBoard, aiTargeted.attackBoard = updateBoards(aiTargeted.opponentShipBoard, aiTargeted.combinedBoard, aiTargeted.attackBoard, row, col)
+    #     print("after next shot targeted")
+    #     print ("ship board is still    "+ aiTargeted.opponentShipBoard)
+    #     print ("combined board is now: " + aiTargeted.combinedBoard)
+    #     print ("attack board is now:   " + aiTargeted.attackBoard)
 
 
     # ai.attackBoard = newAttackBoard
+    
+    aiBest = BattleShipAI()
+    aiBest.attackBoard=blankBoard()
+    # aiBest.opponentShipBoard = "-----------a---------a------------bbbb----------------c---------c---------c--------e-----------ddddd"
+    aiBest.opponentShipBoard = placeShips(5, 10)
+
+    aiBest.opponentCombinedBoard = aiBest.opponentShipBoard
+    aiBest.type = "best"
+    aiBest.boardSize = 10
+    aiBest.numShips = 5
+    aiBest.combinedBoard = aiBest.opponentShipBoard
+
+    # for location in aiBest.attackBoard:
+    while not isWinner(aiBest.combinedBoard):
+        row, col = aiBest.getMove()
+        aiBest.combinedBoard, aiBest.attackBoard = updateBoards(aiBest.opponentShipBoard, aiBest.combinedBoard, aiBest.attackBoard, row, col)
+        print("after next shot targeted")
+        print ("ship board is still    "+ aiBest.opponentShipBoard)
+        print ("combined board is now: " + aiBest.combinedBoard)
+        print ("attack board is now:   " + aiBest.attackBoard)
+
 
 
 
